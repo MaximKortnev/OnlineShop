6aae43e16a632eb4a667bd4257e7b69f74316c1f
max <fafccc@gmail.com>
2024-12-14 15:25:30 +0300

    Добавил мютекс для изменения кеша в SetAsync

--- OnlineShop_WebApp/Services/RedisCacheService.cs
+++ OnlineShop_WebApp/Services/RedisCacheService.cs
@@ -5,26 +5,34 @@ namespace OnlineShop_WebApp.Services
     public class RedisCacheService
     {
         private readonly IConnectionMultiplexer redis;
+        private readonly SemaphoreSlim mutex = new SemaphoreSlim(1, 1);
 
         public RedisCacheService(IConnectionMultiplexer redis) 
         {
             this.redis = redis;
         }
 
-        public async Task SetAsync(string key, string value) 
+        public async Task SetAsync(string key, string value)
         {
-            try {
+            await mutex.WaitAsync();
+            try
+            {
                 var database = redis.GetDatabase();
                 await database.StringSetAsync(key, value);
             }
-            catch 
+            catch
             {
                 return;
+            }
+            finally
+            {
+                mutex.Release();
             }
         }
 
         public async Task<string> TryGetAsync(string key) 
         {
+            
             try 
             {
                 var database = redis.GetDatabase();
